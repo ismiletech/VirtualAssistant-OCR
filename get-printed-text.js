d39a6cdb02ca0e8f0f9619a147036753a60d7002
Shehan Atuk <shehanatukorala@Shehans-MacBook-Pro.local>
2019-06-14 15:05:07 -0400

    first commit, demo get-printed-text.js working

---
+++ get-printed-text.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const request = require('request');
+
+
+//add your subscription key to an env variable called AZURECOMPUTERVISIONSUBSCRIPTIONKEY
+const subscriptionKey = process.env.AZURECOMPUTERVISIONSUBSCRIPTIONKEY;
+
+// You must use the same location in your REST call as you used to get your
+// subscription keys. For example, if you got your subscription keys from
+// westus, replace "westcentralus" in the URL below with "westus".
+const uriBase =
+    'https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/ocr';
+
+const imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/' +
+    'Atomist_quote_from_Democritus.png/338px-Atomist_quote_from_Democritus.png';
+
+// Request parameters.
+const params = {
+    'language': 'unk',
+    'detectOrientation': 'true',
+};
+
+const options = {
+    uri: uriBase,
+    qs: params,
+    body: '{"url": ' + '"' + imageUrl + '"}',
+    headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key' : subscriptionKey
+    }
+};
+
+request.post(options, (error, response, body) => {
+  if (error) {
+    console.log('Error: ', error);
+    return;
+  }
+  let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+  console.log('JSON Response\n');
+  console.log(jsonResponse);
+});
