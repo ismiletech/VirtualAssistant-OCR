599834de83805896e4ecd629e2fb82777a65ec89
Shehan Atuk <shehanatukorala@Shehans-MacBook-Pro.local>
2019-06-28 19:50:09 -0400

    added computer vision, currently logs result to console

---
+++ api/routes/image.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
+const multer = require("multer");
+const path = require("path");
+const url = require("url");
+
+const router = express.Router();
+
+const s3 = new aws.S3({
+  accessKeyId: "AKIAIHDP7GKHX7CS7V2Q",
+  secretAccessKey: "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",
+  Bucket: "flyingfishcattle"
+});
+
+const singleImgUpload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "flyingfishcattle",
+    acl: "public-read",
+    key: function(req, file, cb) {
+      cb(
+        null,
+        path.basename(file.originalname, path.extname(file.originalname)) +
+          "-" +
+          Date.now() +
+          path.extname(file.originalname)
+      );
+    }
+  }),
+
+  limits: { fileSize: 2000000 }, //In bytes: 2000000 bytes = 2mb max file size
+  fileFilter: function(req, file, cb) {
+    checkFileType(file, cb);
+  }
+}).single("image");
+
+function checkFileType(file, cb) {
+  //allowed file types
+  const filetypes = /jpeg|jpg|png|gif/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype); //using regex to check file ext
+
+  if (mimetype && extname) {
+    //checking if file is a valid image
+    return cb(null, true);
+  } else {
+    cb("Error: Images Only!");
+  }
+}
+
+router.post("/upload", (req, res) => {
+  singleImgUpload(req, res, (error) => {
+    if (error) {
+      //return error in json
+      console.log("errors", error);
+      res.json({ error: error });
+    } else {
+      //if file not found
+      if (req.file === undefined) {
+        console.log("Error: No File Selected!");
+        res.json("Error: No File Selected");
+      }
+      //if success
+      const imageName = req.file.key;
+      const imageLocation = req.file.location;
+
+      //Save the file name into the database to img model
+      res.json({
+        image: imageName,
+        location: imageLocation
+      });
+    }
+  });
+  res.send("Successfully uploaded " + req.files.length + " files!");
+  //for uploading multiple files
+});
+
+module.exports = router;
--- get-printed-text.js
+++ get-printed-text.js
@@ -1,7 +1,6 @@
-'use strict';
+"use strict";
 
-const request = require('request');
-
+const request = require("request");
 
 //add your subscription key to an env variable called AZURECOMPUTERVISIONSUBSCRIPTIONKEY
 const subscriptionKey = process.env.AZURECOMPUTERVISIONSUBSCRIPTIONKEY;
@@ -9,34 +8,33 @@ const subscriptionKey = process.env.AZURECOMPUTERVISIONSUBSCRIPTIONKEY;
 // You must use the same location in your REST call as you used to get your
 // subscription keys. For example, if you got your subscription keys from
 // westus, replace "westcentralus" in the URL below with "westus".
-const uriBase =
-    'https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/ocr';
+const uriBase = "https://eastus.api.cognitive.microsoft.com/vision/v2.0/ocr";
 
-const imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/' +
-    'Atomist_quote_from_Democritus.png/338px-Atomist_quote_from_Democritus.png';
+const imageUrl =
+  "https://flyingfishcattle.s3.amazonaws.com/samplePic-1561764480194.jpg";
 
 // Request parameters.
 const params = {
-    'language': 'unk',
-    'detectOrientation': 'true',
+  language: "unk",
+  detectOrientation: "true"
 };
 
 const options = {
-    uri: uriBase,
-    qs: params,
-    body: '{"url": ' + '"' + imageUrl + '"}',
-    headers: {
-        'Content-Type': 'application/json',
-        'Ocp-Apim-Subscription-Key' : subscriptionKey
-    }
+  uri: uriBase,
+  qs: params,
+  body: '{"url": ' + '"' + imageUrl + '"}',
+  headers: {
+    "Content-Type": "application/json",
+    "Ocp-Apim-Subscription-Key": subscriptionKey
+  }
 };
 
 request.post(options, (error, response, body) => {
   if (error) {
-    console.log('Error: ', error);
+    console.log("Error: ", error);
     return;
   }
-  let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
-  console.log('JSON Response\n');
+  let jsonResponse = JSON.stringify(JSON.parse(body), null, "  ");
+  console.log("JSON Response\n");
   console.log(jsonResponse);
 });
