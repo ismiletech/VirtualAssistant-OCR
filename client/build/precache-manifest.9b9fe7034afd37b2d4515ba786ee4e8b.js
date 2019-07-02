52ecd646ac1d31e01d072cb62eb57bd5c42b0887
Shehan Atuk <shehanatukorala@Shehans-MacBook-Pro.local>
2019-07-01 21:42:40 -0400

    first commit

---
+++ client/build/precache-manifest.9b9fe7034afd37b2d4515ba786ee4e8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c218f3a6a20edba8939097b5ee4c4e28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "596d10a4abe87115223a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ab128c39fcefa7bf9484",
+    "url": "/static/js/2.af49309c.chunk.js"
+  },
+  {
+    "revision": "596d10a4abe87115223a",
+    "url": "/static/js/main.a43b44f9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
